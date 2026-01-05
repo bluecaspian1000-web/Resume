8c3704d79975dd164d6f0a9c3addbb438c42585d
UniversityAccount <bluecaspian1000@gmail.com>
2026-01-05 13:57:01 +0330

    Delete SoftProj directory

--- SoftProj/front/js/global.js
+++
@@ -1,6 +0,0 @@
-const API = 'http://127.0.0.1:8000';
-const content = document.getElementById('content');
-
-const token = localStorage.getItem('access_token');
-const user = JSON.parse(localStorage.getItem('user') || '{}');
-const studentId = user.id || null;
--- SoftProj/makemigration.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Applying migrations...
-python manage.py makemigrations courses
-python manage.py makemigrations accounts
-python manage.py makemigrations students
-python manage.py migrate
